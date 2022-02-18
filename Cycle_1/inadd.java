db0c66ffa6783c76a839afd6d429fce6059c9cfa
Duethy Elsa Mathews <77284995+Duethyem02@users.noreply.github.com>
2022-02-18 11:02:56 +0530

    Add files via upload

---
+++ Cycle_1/inadd.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class inadd
+{
+ public static void main(String args[])
+ {
+  Scanner obj=new Scanner(System.in);
+  System.out.println("Enter numbers to be added:");
+  int a=obj.nextInt();
+  int b=obj.nextInt();
+  int sum=a+b;
+  System.out.println("Sum ="+sum);
+
+ }
+}
