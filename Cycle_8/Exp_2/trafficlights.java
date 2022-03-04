26d9c55da23878cb449dcceca5434ea0ad9c977f
Duethy Elsa Mathews <77284995+Duethyem02@users.noreply.github.com>
2022-03-04 09:58:55 +0530

    Exp_8_2

--- Cycle_8/Exp_2/trafficlights.java
+++ Cycle_8/Exp_2/trafficlights.java
@@ -2,19 +2,69 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-class trafficlights extends JFrame implements ActionListener
+class trafficlights extends JFrame implements ItemListener
 {
 	JRadioButton r1,r2,r3;
 	ButtonGroup b = new ButtonGroup();
+	int x=0,y=0,z=0;
+		
+	public void itemStateChanged(ItemEvent ie)
+	{
+	 	if (ie.getSource()==r1){
+	 		if(ie.getStateChange()==1){
+	 			x=1;
+	 			repaint();
+	 		}
+	 	}
+	 	if (ie.getSource()==r2){
+	 		if(ie.getStateChange()==1){
+	 			y=1;
+	 			repaint();
+	 		}
+	 	}
+	 	if (ie.getSource()==r3){
+	 		if(ie.getStateChange()==1){
+	 			z=1;
+	 			repaint();
+	 		}
+	 	}
+	}
 	
 	public void paint(Graphics g)
 	{
-		
+	 g.drawRect(200,200,50,150);
+	 g.setColor(Color.BLACK);
+	 g.fillRect(200,200,50,150);
+	 g.drawOval(205,205,40,40);
+	 g.drawOval(205,255,40,40);
+	 g.drawOval(205,305,40,40);
+	 if(x==1){
+	 	g.setColor(Color.RED);
+	 	g.fillOval(205,205,40,40);
+	 	g.setColor(Color.WHITE);
+	 	g.fillOval(205,255,40,40);
+	 	g.setColor(Color.WHITE);
+	 	g.fillOval(205,305,40,40);
+	 	x=0;
 	}
-		
-	public void actionPerformed(ActionEvent e)
-	{
-	 
+	if(y==1){
+	 	g.setColor(Color.WHITE);
+	 	g.fillOval(205,205,40,40);
+	 	g.setColor(Color.YELLOW);
+	 	g.fillOval(205,255,40,40);
+	 	g.setColor(Color.WHITE);
+	 	g.fillOval(205,305,40,40);
+	 	y=0;
+	}
+	if(z==1){
+	 	g.setColor(Color.WHITE);
+	 	g.fillOval(205,205,40,40);
+	 	g.setColor(Color.WHITE);
+	 	g.fillOval(205,255,40,40);
+	 	g.setColor(Color.GREEN);
+	 	g.fillOval(205,305,40,40);
+	 	z=0;
+	}
 	}
 	
 	trafficlights()
@@ -23,13 +73,13 @@ class trafficlights extends JFrame implements ActionListener
 	 	setLayout(new FlowLayout(FlowLayout.RIGHT));
 	 	
 	 	r1 = new JRadioButton("RED");
-		r1.addActionListener(this);
+		r1.addItemListener(this);
 		
 	 	r2 = new JRadioButton("YELLOW");
-	 	r2.addActionListener(this);
+	 	r2.addItemListener(this);
 	 	
 	 	r3 = new JRadioButton("GREEN");
-	 	r3.addActionListener(this);
+	 	r3.addItemListener(this);
 	 	
 	 	add(r1);add(r2);add(r3);
 	 	
